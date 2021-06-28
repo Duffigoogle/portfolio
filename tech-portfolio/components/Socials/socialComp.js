8f093e41038080e3ebfc38c90450fb271e476e1e
Caleb Duff <calebdduff@gmail.com>
2021-06-28 14:44:05 +0100

    added socialIcons Component

---
+++ tech-portfolio/components/Socials/socialComp.js
@@ -0,0 +1,49 @@
+import React from "react";
+import Icon from '../common/icons/icons';
+
+const SocialsComp = () => {
+  return (
+    <SocialsContainer>
+      <SocialsLink
+        href="https://github.com/duffigoogle"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Icon name='Github' size={15} color='red' />
+      </SocialsLink>
+      <SocialsLink
+        href="https://twitter.com/duffygoogle"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Icon name='Twitter' size={15} color='red' />
+      </SocialsLink>
+      <SocialsLink
+        href="https://www.linkedin.com/in/caleb-duff/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Icon name='Linkedin' size={15} color='red' />
+      </SocialsLink>
+
+    </SocialsContainer>
+  );
+};
+
+export default SocialsComp;
+
+
+
+//styled-components for SocialsComp
+
+const SocialsContainer = styled.div`
+    display: flex;
+`
+
+const SocialsLink = styled.a`
+  text-decoration: none;
+  &:hover {
+    color: skyblue;
+  }
+
+`
