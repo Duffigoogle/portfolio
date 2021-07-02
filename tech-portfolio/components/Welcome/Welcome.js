6306ae903d4a78b021608c9f523e57573a46188b
Caleb Duff <calebdduff@gmail.com>
2021-07-02 13:30:04 +0100

    Wecome Comp added

--- tech-portfolio/components/Welcome/Welcome.js
+++ tech-portfolio/components/Welcome/Welcome.js
@@ -24,28 +24,54 @@ export default WelcomeComp;
 // styled components for WelcomeComp
 
 const WelcomeCompContainer = styled.div`
-    width: 100%;
-    border: 3px solid red;
-    /* border: 2px border green; */
+    width: 81vw;
+    border: 2px solid red;
+    margin-top: 80px;
+    /* height: 75vh; */
 
 `
 
-const WeclomeHeading1 = styled.h1`
+const WeclomeHeading1 = styled.h2`
     color: skyblue;
     font-weight: 800;
-    font-size: 5rem;
+    font-size: 3.5rem;
     text-align: center;
+    span {
+        color: orange;
+        font-size: 5rem;
+        &:hover {
+            color: orangered;
+        }
+    }
 
 `
 const WelcomeButton = styled.button`
-    border-radius: 15px;
-    background-color: orange;
-    padding: 10px 15px;
+    width: 280px;
+    text-transform: uppercase;
+    height: 45px;
+    font-family: 'Roboto', sans-serif;
+    font-size: 1.1rem;
+    text-transform: uppercase;
+    letter-spacing: 2.5px;
+    font-weight: 500;
+    background-color: #d1d3bd;
+    color: #000;
+    padding: 0 0.6rem;
     display: flex;
     align-items: center;
+    justify-content: center;
     /* box-shadow: ; */
-    margin: 35px auto;
+    margin: 60px auto 25px;
     outline: none;
-    font-size: 1.2rem;
-    /* width: 200px; */
+    border: none;
+    border-radius: 45px;
+    box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.1);
+    transition: all 0.3s ease 0s;
+    cursor: pointer;
+    &:hover {
+        background-color: #2EE59D;
+        box-shadow: 0px 15px 20px rgba(46, 229, 157, 0.4);
+        color: #fff;
+        transform: translateY(-7px);
+    }
 `
