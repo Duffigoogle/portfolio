8ecfbc236c1f640d15c14bf2b223071be1e116db
Caleb Duff <calebdduff@gmail.com>
2021-07-15 17:01:54 +0100

    Added popup Modal component

---
+++ tech-portfolio/components/Form/Modal.js
@@ -0,0 +1,16 @@
+const ModalComp = () => {
+    return (
+        <div>
+            <div>
+              <p> Duffigoogle (Github)</p>
+              {/* ArrowRight */}
+            </div>
+            <div>
+                <p>Turn on Settings Sync...</p> 
+            </div>
+
+        </div>
+    )
+}
+
+export default ModalComp;
