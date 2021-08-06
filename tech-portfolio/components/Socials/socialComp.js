0a7a7db97de821a79ff6e130782ff1fa6a767fed
Caleb Duff <calebdduff@gmail.com>
2021-08-06 10:30:26 +0100

    added Socialmedia Comp

--- tech-portfolio/components/Socials/socialComp.js
+++ tech-portfolio/components/Socials/socialComp.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Icon from '../common/icons/icons';
+import Icon from "../common/icons/icons";
 import styled from "styled-components";
 
 const SocialsComp = () => {
@@ -10,41 +10,43 @@ const SocialsComp = () => {
         target="_blank"
         rel="noopener noreferrer"
       >
-        <Icon name='Github' size={25} color='red' />
+        <Icon name="GithubIcon" size={20} color="#cec" />
       </SocialsLink>
       <SocialsLink
         href="https://twitter.com/duffygoogle"
         target="_blank"
         rel="noopener noreferrer"
       >
-        <Icon name='Twitter' size={25} color='red' />
+        <Icon name="twitter" size={20} color="skyblue" />
       </SocialsLink>
       <SocialsLink
         href="https://www.linkedin.com/in/caleb-duff/"
         target="_blank"
         rel="noopener noreferrer"
       >
-        <Icon name='Linkedin' size={25} color='red' />
+        <Icon name="linkedin" size={20} color="#fff" />
       </SocialsLink>
-
     </SocialsContainer>
   );
 };
 
 export default SocialsComp;
 
-
-
 //styled-components for SocialsComp
 
 const SocialsContainer = styled.div`
-    display: flex;
-`
+  display: flex;
+  /* border: 1px solid yellow; */
+  min-height: 1.7rem;
+  width: 6rem;
+  justify-content: space-between;
+  align-items: center;
+`;
 
 const SocialsLink = styled.a`
   text-decoration: none;
+
   &:hover {
     color: skyblue;
   }
-
-`
+`;
