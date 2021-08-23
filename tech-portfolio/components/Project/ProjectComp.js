c56c8b33683e9265cf722658d65c9d1ef7484a47
Caleb Duff <calebdduff@gmail.com>
2021-08-23 20:45:01 +0100

    slice and map projects

--- tech-portfolio/components/Project/ProjectComp.js
+++ tech-portfolio/components/Project/ProjectComp.js
@@ -37,5 +37,39 @@ const PortfolioWorksContainer = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
-  height: 120vh;
+  height: 125vh;
+  background-color: #000;
 `;
+
+export const ProjectCollectionOne =() => {
+    const { elementRef } = useContext(CalculateHeightContext);
+
+  return (
+    <>
+      <PortfolioWorksContainer ref={elementRef}>
+        {projects.slice(0, 3).map((project, index) => (
+          <ProjectSchema key={index} {...project} id={index} />
+        ))}
+      </PortfolioWorksContainer>
+    </>
+
+  )
+};
+
+
+export const ProjectCollectionTwo =() => {
+    const { elementRef } = useContext(CalculateHeightContext);
+
+  return (
+    <>
+      <PortfolioWorksContainer ref={elementRef}>
+        {projects.slice(3, 6).map((project, index) => (
+          <ProjectSchema key={index} {...project} id={index} />
+        ))}
+      </PortfolioWorksContainer>
+    </>
+
+  )
+}
+
+
--- tech-portfolio/components/Project/Project_Schema.js
+++ tech-portfolio/components/Project/Project_Schema.js
@@ -1,99 +1,108 @@
 import React from "react";
-import Icon from '../common/icons/icons';
-import styled from 'styled-components';
+import Icon from "../common/icons/icons";
+import styled from "styled-components";
 
-export default function ProjectSchema({ id, title, description, github, live, stack, isPrivate }) {
+export default function ProjectSchema({
+  id,
+  title,
+  description,
+  github,
+  live,
+  stack,
+  isPrivate,
+}) {
   return (
-            <ProjectContainer>
-                <ProjectBox>
-                        <ProjectDetails>
-                            <ProjectTitle> {title} </ProjectTitle>
-                            <ProjectTextDescription> {description} </ProjectTextDescription>
-                            <ProjectStackSmall> {stack} </ProjectStackSmall>
-                        </ProjectDetails>
+    <ProjectContainer>
+      <ProjectBox>
+        <ProjectDetails>
+          <ProjectTitle> {title} </ProjectTitle>
+          <ProjectTextDescription> {description} </ProjectTextDescription>
+          <ProjectStackSmall> {stack} </ProjectStackSmall>
+        </ProjectDetails>
 
-                        <ProjectLinks>
-                            <small>
-                                <ProjectLink href={live} target="_blank" rel="noopener noreferrer">
-                                    Live {" "}<Icon name='EyeIcon' size={20} color='red' className='icon'/> 
-                                </ProjectLink>
-                            </small>
+        <ProjectLinks>
+          <small>
+            <ProjectLink href={live} target="_blank" rel="noopener noreferrer">
+              Live{" "}
+              <Icon name="EyeIcon" size={20} color="red" className="icon" />
+            </ProjectLink>
+          </small>
 
-                            {isPrivate ? (
-                                    ""
-                                    ) : (
-                                <small>
-                                    <ProjectLink href={github} target="_blank" rel="noopener noreferrer">
-                                        {" "}
-                                        Code {" "}<Icon name='CodeIcon' size={20} color='red' className='icon'/> 
-                                    </ProjectLink>
-                                </small>
-                            )}
-                        </ProjectLinks>
-                </ProjectBox>
-            </ProjectContainer>
-
-  )
-};
-
+          {isPrivate ? (
+            ""
+          ) : (
+            <small>
+              <ProjectLink
+                href={github}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {" "}
+                Code{" "}
+                <Icon name="CodeIcon" size={20} color="red" className="icon" />
+              </ProjectLink>
+            </small>
+          )}
+        </ProjectLinks>
+      </ProjectBox>
+    </ProjectContainer>
+  );
+}
 
 //styled components for ProjectSchema
 
 const ProjectContainer = styled.div`
-    width: 70%;
-    background-color: #16161a;
-    margin: 1% auto 1.8%;
-    position: relative;
-    transition: all 0.2s;
-    box-shadow: 0px 2px 7px #e0e0e0;
-`
+  width: 70%;
+  background-color: #16161a;
+  margin: 1% auto 1.8%;
+  position: relative;
+  transition: all 0.2s;
+  box-shadow: 0px 2px 7px #e0e0e0;
+`;
 
 const ProjectBox = styled.div`
-    display: flex;
-    flex-direction: column;
-    padding: 6%;
-    flex-grow: 1;
-    justify-content: space-between;
-    cursor: default;
-`
+  display: flex;
+  flex-direction: column;
+  padding: 6%;
+  flex-grow: 1;
+  justify-content: space-between;
+  cursor: default;
+`;
 
-const ProjectDetails = styled.div`
-
-`
+const ProjectDetails = styled.div``;
 
 const ProjectTitle = styled.h3`
-    margin: 0;
-    font-size: 1.5rem;
-    color: #e0e0e0;
-`
+  margin: 0;
+  font-size: 1.5rem;
+  color: #e0e0e0;
+`;
 
 const ProjectStackSmall = styled.small`
-    margin-bottom: 13px;
-    font-size: smaller;
-    color: grey;
-`
+  margin-bottom: 13px;
+  font-size: smaller;
+  color: gray;
+`;
 
 const ProjectTextDescription = styled.p`
-    font-size: 1rem;
-    line-height: 2;
-    color: #777778;
-    height: 5rem;
-    font-weight: 500;
-`
+  font-size: 1rem;
+  line-height: 2;
+  color: #777778;
+  height: 5rem;
+  font-weight: 500;
+`;
 const ProjectLink = styled.a`
-    display: flex;
-    font-size: 0.8rem;
-    line-height: 1.4rem;
-    
-    text-decoration: none;
-    margin-right: 0.8rem;
-    color: grey;
+  display: flex;
+  font-size: 0.8rem;
+  line-height: 1.4rem;
 
-    .icon {
-        margin-left: 8px;
-    }
-`
+  text-decoration: none;
+  margin-right: 0.8rem;
+  color: gray;
+
+  .icon {
+    margin-left: 8px;
+  }
+`;
 const ProjectLinks = styled.div`
-    display: flex;
-    
-`
+  display: flex;
+`;
